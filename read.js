c5773352b405949b97b31e841eb6be83850a7121
Birdie Wu <wnn1120@gmail.com>
2023-08-03 18:19:13 +0800

    todo list

---
+++ read.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+var colors = require('colors');
+
+fs.readFile('todos.json', function(err, data) {
+    let todos = JSON.parse(data);
+    let todolist = '您的待辦事項：';
+    for (let [index, todo] of todos.entries()) {
+        todolist = todolist + `\n#${index} ${todo.title}`;
+    }
+    console.log(colors.bold(todolist));
+    process.exit(0);
+});
