69c72415b03c66f57f4cba9e2e73b224fd1c0299
Birdie Wu <wnn1120@gmail.com>
2023-08-03 19:22:46 +0800

    sync

--- create.js
+++ create.js
@@ -1,18 +1,34 @@
+// var fs = require('fs');
+
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// fs.readFile('todos.json', function(err, data) {
+//     readline.question('您要新增什麼待辦事項？\n', function (result) {
+//         let todos = JSON.parse(data);
+//         let newTodo = { title : result };
+//         todos.push(newTodo);
+//         fs.writeFile('todos.json', JSON.stringify(todos), function (err) {
+//             console.log(`新增事項：${result}`);
+//             process.exit(0);
+//         });
+//     });
+// });
+
 var fs = require('fs');
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const readlineSync = require('readline-sync');
 
-fs.readFile('todos.json', function(err, data) {
-    readline.question('您要新增什麼待辦事項？\n', function (result) {
-        let todos = JSON.parse(data);
-        let newTodo = { title : result };
-        todos.push(newTodo);
-        fs.writeFile('todos.json', JSON.stringify(todos), function (err) {
-            console.log(`新增事項：${result}`);
-            process.exit(0);
-        });
-    });
-});
+let todos = JSON.parse(fs.readFileSync("todos.json"));
+
+let result = readlineSync.question('您要新增什麼待辦事項？\n')
+
+let newTodo = { title : result };
+
+todos.push(newTodo);
+
+fs.writeFileSync("todos.json", JSON.stringify(todos));
+
+console.log(`新增事項：${result}`);
--- read.js
+++ read.js
@@ -1,13 +1,24 @@
+// var fs = require('fs');
+
+// var colors = require('colors');
+
+// fs.readFile('todos.json', function(err, data) {
+//     let todos = JSON.parse(data);
+//     let todolist = '您的待辦事項：';
+//     for (let [index, todo] of todos.entries()) {
+//         todolist = todolist + `\n#${index} ${todo.title}`;
+//     }
+//     console.log(colors.bold(todolist));
+//     process.exit(0);
+// });
+
 var fs = require('fs');
 
-var colors = require('colors');
+let todos = JSON.parse(fs.readFileSync("todos.json"));
 
-fs.readFile('todos.json', function(err, data) {
-    let todos = JSON.parse(data);
-    let todolist = '您的待辦事項：';
-    for (let [index, todo] of todos.entries()) {
-        todolist = todolist + `\n#${index} ${todo.title}`;
-    }
-    console.log(colors.bold(todolist));
-    process.exit(0);
-});
+let todolist = '您的待辦事項：';
+
+for (let [index, todo] of todos.entries()) {
+    todolist = todolist + `\n#${index} ${todo.title}`;
+}
+console.log(todolist);
